4ee323d038b165d5a742d77b04a8dbf4e3a1e38a
Andrew <amarc20@wgu.edu>
2024-05-27 11:41:42 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from faker import Faker
+
+fake = Faker()
+
+print(fake.name())
+print(fake.address())
+print(fake.text())
+print(fake.email())
+print(fake.phone_number())
+print(fake.state())
+print("United States")
+
+
+
